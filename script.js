cd3a4ef61fd09f2ec0fdb66a4233a570cf451a30
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2023-01-18 20:44:07 +0000

    Added validation code for password length on script.js file

--- script.js
+++ script.js
@@ -90,7 +90,25 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var passLenght = parseInt(prompt("Add the length of the Password. Between 8-128",""))
+  var charTypes = [["Lowercase Characters",lowerCasedCharacters], ["Uppercase Characters",upperCasedCharacters], ["Numeric Characters",numericCharacters], ["Special Characters ($@%&*, etc)",specialCharacters]];
+  
+  //Validate the password's length and prompts the user to input the correct criteria
+  while (passLenght<8 || passLenght>128){
+    if (passLenght<8){
+      passLenght = parseInt(prompt("Your password MUST have 8 or more characters\nPlease try again",""));
+    }
+    else if (passLenght>128){
+      passLenght = parseInt(prompt("Your password MUST have 128 characters at most\nPlease try again",""));
+    }
+  }
+  
+  // for (i = 0; i < charTypes.length; i++){
 
+  // }
+  var d = passLenght * 2;
+  console.log(charTypes[0][1]);
+  console.log(passLenght);
 }
 
 // Function for getting a random element from an array
@@ -100,7 +118,8 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  getPasswordOptions()
+  
 }
 
 // Get references to the #generate element
@@ -116,3 +135,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
